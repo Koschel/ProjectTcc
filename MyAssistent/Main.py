5ab38e50029f635d05c0b76229f571eb0f72e9a4
Diego Koschel <dasksoethe1@gmail.com>
2019-10-13 19:54:48 -0300

    Trainner

--- MyAssistent/Main.py
+++ MyAssistent/Main.py
@@ -9,11 +9,16 @@ import pyttsx3 as pytts
 bot = ChatBot('Charle')
 speak = pytts.init('sapi5')
 
-conv = ['Olá',
-        'Oi',
-        'Qual e o seu nome?',
-        'Meu nome é Charler',
-        'O que você gosta de fazer?']
+conv = ['começar Estudos',
+        'tópico um existente: HTML',
+        'começar',
+        'tópico 1 existente: HTML']
+
+conv = ['topico um',
+        'HTML (abreviação para a expressão inglesa HyperText Markup Language, que significa Linguagem de Marcação de Hipertexto) é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML HyTime é um padrão para a representação estruturada de hipermídia e conteúdo baseado em tempo. Um documento é visto como um conjunto de eventos concorrentes dependentes de tempo (como áudio, vídeo, etc.), conectados por hiperligações. O padrão é independente de outros padrões de processamento de texto em geral',
+        'topico 1',
+        'HTML (abreviação para a expressão inglesa HyperText Markup Language, que significa Linguagem de Marcação de Hipertexto) é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML HyTime é um padrão para a representação estruturada de hipermídia e conteúdo baseado em tempo. Um documento é visto como um conjunto de eventos concorrentes dependentes de tempo (como áudio, vídeo, etc.), conectados por hiperligações. O padrão é independente de outros padrões de processamento de texto em geral'
+        ]
 
 trainer = ListTrainer(bot)
 
