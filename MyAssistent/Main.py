9298183882daf179539f94156631ec571e12808c
Diego Koschel <dasksoethe1@gmail.com>
2019-11-17 19:54:36 -0300

    Tcc

--- MyAssistent/Main.py
+++ MyAssistent/Main.py
@@ -2,6 +2,7 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
+
 import speech_recognition as sr
 import pyttsx3 as pytts
 
@@ -9,23 +10,20 @@ import pyttsx3 as pytts
 bot = ChatBot('Charle')
 speak = pytts.init('sapi5')
 
-conv = ['iniciar estudos',
-        'topicos existentes: Topico 1 conceito HTML, qual você deseja',
-        'topico 1',
-        'HTML (abreviação para a expressão inglesa HyperText Markup Language, que significa Linguagem de Marcação de Hipertexto) é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML HyTime é um padrão para a representação estruturada de hipermídia e conteúdo baseado em tempo. Um documento é visto como um conjunto de eventos concorrentes dependentes de tempo (como áudio, vídeo, etc.), conectados por hiperligações. O padrão é independente de outros padrões de processamento de texto em geral'
-        ]
-
-
+conv = open('chatterbot_HTML.txt', 'r', encoding='utf-8').readlines()
 trainer = ListTrainer(bot)
-
 trainer.train(conv)
+
+#trainer.train(conv)
 
 def Speak(text):
     speak.say(text)
     speak.runAndWait()
 
 def recognize():
-    print('Escutando...')
+    msgInicial = 'Para verificar quais tópicos de estudo você possui fale o comando "Iniciar estudos", para parar o fale o comando Parar, O que você deseja fazer ?'
+    print('Charle' + msgInicial)
+    Speak(msgInicial)
     rec = sr.Recognizer()
     with sr.Microphone() as mp:
         rec.adjust_for_ambient_noise(mp)
@@ -35,11 +33,19 @@ def recognize():
                 audio = rec.listen(mp)
                 user_input = rec.recognize_google(audio, language="pt")
                 print('Você: ', user_input)
+                if user_input == 'Nao' or user_input == 'nao' or user_input == 'não' or user_input == 'Não' or user_input == 'n' or user_input == 'nada' or user_input == 'Nada' or user_input == 'Parar' or user_input== 'parar':
+                    mgsSair = 'Acabamos por aqui, até logo'
+                    print(mgsSair)
+                    Speak(mgsSair)
+                    break
                 bot_response = bot.get_response(user_input)
-                print('Bot:', bot_response)
+                print('Charle:', bot_response)
                 Speak(bot_response)
+
             except sr.UnknownValueError:
-                return "Desculpe, não entendi"
+                erro = "Desculpe, não entendi"
+                print(erro)
+                Speak(erro)
 
 
 speech = recognize()
